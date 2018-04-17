b315f9af99af141d3305c42b2481138513285c38
nghiat <trantuannghia95@gmail.com>
2018-04-17 10:59:44 +0700

    Fix 80 columns comment

--- find_libclang.py
+++ find_libclang.py
@@ -1,9 +1,11 @@
-##-----------------------------------------------------------------------------##
-## find_libclang.py                                                            ##
-##                                                                             ##
-## This file is distributed under the MIT License. See LICENSE.txt for details.##
-## Copyright (C) Tran Tuan Nghia <trantuannghia95@gmail.com> 2018              ##
-##-----------------------------------------------------------------------------##
+##----------------------------------------------------------------------------##
+## find_libclang.py                                                           ##
+##                                                                            ##
+## This file is distributed under the MIT License.                            ##
+## See LICENSE.txt for details.                                               ##
+## Copyright (C) Tran Tuan Nghia <trantuannghia95@gmail.com> 2018             ##
+##----------------------------------------------------------------------------##
+
 import os
 import sys
 
